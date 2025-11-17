7d0fa7d2c13d16c046d6adce5f9d3acdbc7e2079
umidjon11111 <umidjongafforov844@gmail.com>
2025-11-17 13:23:42 +0500

    aap

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://ticket-app-topaz.vercel.app",
     methods: ["GET", "POST"],
   },
 });
